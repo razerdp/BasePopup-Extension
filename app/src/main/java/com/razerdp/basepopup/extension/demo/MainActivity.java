c454614591b236ed86a13d0105c5f656f4dfabb9
razerdp <razerdp123@gmail.com>
2020-04-07 23:48:42 +0800

    //first commit

---
+++ app/src/main/java/com/razerdp/basepopup/extension/demo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.razerdp.basepopup.extension.demo;
+
+import android.os.Bundle;
+
+import com.razerdp.basepopup.extension.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
